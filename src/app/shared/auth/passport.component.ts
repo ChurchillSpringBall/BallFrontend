29eba328435be88a515d2050b23877daf3b4bbff
Liam Gray <hoxxep@gmail.com>
2016-10-27 15:49:25 +0100

    Try google loader design

--- src/app/shared/auth/passport.component.ts
+++ src/app/shared/auth/passport.component.ts
@@ -1,37 +1,28 @@
-import {AccessToken, SDKToken} from '../sdk/models';
+import {SDKToken} from '../sdk/models';
 import {LoopBackAuth} from '../sdk/services';
 import {Component} from '@angular/core';
-import {Router, ActivatedRoute} from '@angular/router';
+import {Router} from '@angular/router';
 import {CookieService} from 'angular2-cookie/core';
 
+/**
+ * Module to load the cookies set by loopback-component-passport into the Angular2 Loopback SDK format...
+ */
 @Component({
-  template: `<span>redirecting...</span>`
+  template: `<span>Redirecting...</span>`
 })
 export class PassportComponent {
   constructor(private auth: LoopBackAuth,
-              private route: ActivatedRoute,
               private router: Router,
               private cookies: CookieService) {
-    // this.route.params.subscribe((token: AccessToken) => {
-    //   if (token.id && token.userId) {
-    //     const sdkToken: SDKToken = new SDKToken(token);
-    //     this.auth.setUser(sdkToken);
-    //     this.auth.setRememberMe(true);
-    //     this.auth.save();
-    //     this.router.navigate(['/']);
-    //   }
-    // });
     const sdkToken: SDKToken = new SDKToken();
 
     sdkToken.id = this.cookies.get('access_token');
     sdkToken.userId = this.cookies.get('userId');
 
-    console.log(sdkToken, this.cookies.get('userId'), this.cookies);
-
     this.auth.setUser(sdkToken);
     this.auth.setRememberMe(true);
     this.auth.save();
-    this.router.navigate(['/']);
 
+    this.router.navigate(['/']);
   }
 }
